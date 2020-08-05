5acaf9eb8d0bd12b4021dd280f447e3414a6278b
himrah <rkrjob55@gmail.com>
2020-08-05 23:44:43 +0530

    models update

--- app/models.py
+++ app/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    image = models.ImageField(upload_to="category/",blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=80)
+    mrp = models.FloatField()
+    list_price = models.FloatField()
+    discount = models.FloatField()
+    qty = models.IntegerField()
+    moq = models.IntegerField(default=1,blank=True)
+    inactive = models.BooleanField(default=True,blank=True)
+    instock = models.BooleanField(default=False,blank=True)
+    sort_desc = models.TextField(null=True,blank=True)
+    long_desc = models.TextField(null=True,blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    seller = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class ProductImages(models.Model):
+    large_image = models.ImageField(upload_to='product/large',null=True,blank=True)
+    normal_image = models.ImageField(upload_to='product/normal',null=True,blank=True)
+    thumbnail_image = models.ImageField(upload_to='product/thumbnail',null=True,blank=True)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.product.name
+
+    def save(self,force_insert=False,force_update=False, using=None):
+        im = Image.open(self.large_image)
+        output = BytesIO()
+        if im.size[0]<=700:
+            basewidth = im.size[0]
+        else:
+            basewidth = 600
+
+        wpercent = (basewidth/float(im.size[0]))
+        hsize = int((float(im.size[1])*float(wpercent)))
+        im = im.resize((basewidth,hsize), Image.ANTIALIAS)
+        im = im.convert("RGB")
+        self.height = im.height
+        self.width = im.width
+        im.save(output, format='JPEG', quality=70)
+        self.normal_image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.large_image.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+
+        if im.size[0]<=100:
+            basewidth = im.size[0]
+        else:
+            basewidth = 100
+        wpercent = (basewidth/float(im.size[0]))
+        hsize = int((float(im.size[1])*float(wpercent)))
+        imc = im.resize((basewidth,hsize), Image.ANTIALIAS)
+        imc = imc.convert("RGB")
+        output = BytesIO()
+        imc.save(output, format='JPEG', quality=70)
+        output.seek(0)
+        self.thumbnail_image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.large_image.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+        super(ProductImages,self).save()
+    
--- xgrosr/settings.py
+++ xgrosr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+&y0w!l%kf^7l0e16swnyo8!^$8ny0x4q2b-i-)ow$6k%c+pa0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'xgrosr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
