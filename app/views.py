09c505450b9d6c29878b9906175b4bcec7b953a6
devendermathu <rahulmath846@gmail.com>
2020-08-08 11:12:19 +0530

    update

--- app/views.py
+++ app/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect, HttpResponse
 from app.models import *
 # Create your views here.
+
+def Home_page(request):
+    return render(request,'home.html')
+
+
+def category_page(request):
+    return render (request,'category.html')
--- xgrosr/urls.py
+++ xgrosr/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Home_page,name="Home_page"),
+    path('category/',category_page,name='category_page')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
